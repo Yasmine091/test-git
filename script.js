2249717b9bc11ce32b3f53e320f1fc138203f083
Yasmine091 <yasmine.emdounan@gmail.com>
2020-12-01 11:49:16 +0100

    Y - 3 musiques

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function song() {
-    return 'My inmortal';
+function song1() {
+    return 'Bring me to life';
 }
 
 console.log(song());
+
+function song2() {
+    return 'My inmortal';
+}
+
+console.log(song2());
+
+function song3() {
+    return 'Everybody\'s fool';
+}
+
+console.log(song3());
