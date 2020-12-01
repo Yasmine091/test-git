bc088669eb0245122ff58c629f4f04c2c7fa0e88
Yasmine091 <yasmine.emdounan@gmail.com>
2020-12-01 12:02:15 +0100

    Y - Ajout de contenu dans les fonctions + musique

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 function song1() {
-    return 'Bring me to life';
+    var name = 'Bring me to life';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
 }
 
 console.log(song());
 
 function song2() {
-    return 'My inmortal';
+    var name = 'My inmortal';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
 }
 
 console.log(song2());
 
 function song3() {
-    return 'Everybody\'s fool';
+    var name = 'Everybody\'s fool';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
 }
 
 console.log(song3());
 
 function song4() {
-    return 'Good enough';
+    var name = 'Good enough';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
 }
 
 console.log(song4());
+
+function song5() {
+    var name = 'My heart is broken';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
+}
+
+console.log(song5());
+
+function song6() {
+    var name = 'Imperfection';
+    var group = 'Evanescence';
+    return 'Now listening to : ' + name + ' from ' + group;
+}
+
+console.log(song6());
